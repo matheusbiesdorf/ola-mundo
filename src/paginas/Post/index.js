1a6bc7e5ccd0eb728da8205634aa840ec6538b0c
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-01-14 12:48:18 -0300

    Implementação de página não encontrada

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,9 +1,28 @@
-import { useParams } from "react-router-dom"
+import './Post.css'
+import { useParams } from "react-router-dom";
+import posts from "json/posts.json";
+import PostModelo from "componentes/PostModelo";
+import ReactMarkdown from "react-markdown";
 
+export default function Post() {
+    const parametros = useParams();
 
-export default function Post(){
-    const parametros = useParams()
+    const post = posts.find((post) => {
+        return post.id === Number(parametros.id);
+    });
+
+    if(!post){
+        return <h1>Post não encontrado</h1>
+    }
+
     return (
-        <h1>Post {parametros.id}</h1>
-    )
+        <PostModelo
+            FotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
+        >
+            <div className="post-markdown-container">
+                <ReactMarkdown>{post.texto}</ReactMarkdown>
+            </div>
+        </PostModelo>
+    );
 }
