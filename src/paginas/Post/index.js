2656fa9d6ea51d10b3236ea173f330f5a962ee4b
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-01-14 18:12:02 -0300

    Projeto concluído

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,9 +1,10 @@
-import './Post.css'
-import { useParams } from "react-router-dom";
+import "./Post.css";
+import { Route, Routes, useParams } from "react-router-dom";
 import posts from "json/posts.json";
 import PostModelo from "componentes/PostModelo";
 import ReactMarkdown from "react-markdown";
-import NaoEncontrada from 'assets/NaoEncontrada';
+import NaoEncontrada from "assets/NaoEncontrada";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 export default function Post() {
     const parametros = useParams();
@@ -12,18 +13,27 @@ export default function Post() {
         return post.id === Number(parametros.id);
     });
 
-    if(!post){
-        return <NaoEncontrada />
+    if (!post) {
+        return <NaoEncontrada />;
     }
 
     return (
-        <PostModelo
-            FotoCapa={`/assets/posts/${post.id}/capa.png`}
-            titulo={post.titulo}
-        >
-            <div className="post-markdown-container">
-                <ReactMarkdown>{post.texto}</ReactMarkdown>
-            </div>
-        </PostModelo>
+        <Routes>
+            <Route path="*" element={<PaginaPadrao />}>
+                <Route
+                    index
+                    element={
+                        <PostModelo
+                            FotoCapa={`/assets/posts/${post.id}/capa.png`}
+                            titulo={post.titulo}
+                        >
+                            <div className="post-markdown-container">
+                                <ReactMarkdown>{post.texto}</ReactMarkdown>
+                            </div>
+                        </PostModelo>
+                    }
+                />
+            </Route>
+        </Routes>
     );
 }
